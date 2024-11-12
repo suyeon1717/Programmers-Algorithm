7ff9b6f3b4417d8fc0f969518b1435f4662af81e
Suyeoneee <101303761+suyeon1717@users.noreply.github.com>
2024-11-12 11:46:58 +0900

    Create Q14 나머지가 1이 되는 수 찾기.java

---
+++ Q14 나머지가 1이 되는 수 찾기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int x = 1;
+        
+        while(n % x !=1)
+            x++;
+        
+        answer = x;
+        return answer;
+    }
+}
